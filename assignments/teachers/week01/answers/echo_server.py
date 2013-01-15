cd953a77bc7a16cbb3da7171e6788afdfb407265
cewing <cris@crisewing.com>
2013-01-14 22:25:25 -0800

    exit cleanly from server on ctrl-c

--- assignments/teachers/week01/answers/echo_server.py
+++ assignments/teachers/week01/answers/echo_server.py
@@ -12,25 +12,31 @@ sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
 
-while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+try:
 
-    try:
-        print >>sys.stderr, 'connection from', client_address
+    while True:
+        # Wait for a connection
+        print >>sys.stderr, 'waiting for a connection'
+        connection, client_address = sock.accept()
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
-            if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.sendall(data)
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
+        try:
+            print >>sys.stderr, 'connection from', client_address
 
-    finally:
-        # Clean up the connection
-        connection.close()
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                print >>sys.stderr, 'received "%s"' % data
+                if data:
+                    print >>sys.stderr, 'sending data back to the client'
+                    connection.sendall(data)
+                else:
+                    print >>sys.stderr, 'no more data from', client_address
+                    break
+
+        finally:
+            # Clean up the connection
+            connection.close()
+
+except KeyboardInterrupt:
+    sock.close()
+    sys.exit(0)
