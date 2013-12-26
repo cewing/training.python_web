b08677bfdb49ee5a2d1d47a9443f3fd437dc1c6d
cewing <cris@crisewing.com>
2013-12-26 16:28:00 -0700

    add a few more utility methods.

--- resources/session01/session1.py
+++ resources/session01/session1.py
@@ -1,10 +1,30 @@
 import socket
 
 
+def get_named_services(first_port=0, last_port=2**15+2**14):
+    tmpl = '{0: >4}: {1}'
+    for i in xrange(first_port, last_port):
+        try:
+            service = socket.getservbyport(i)
+        except socket.error:
+            continue
+        print tmpl.format(i, service)
+
+
 def get_constants(prefix):
-    return dict( (getattr(socket, n), n)
-                 for n in dir(socket)
-                 if n.startswith(prefix))
+    return dict( 
+        (getattr(socket, n), n)
+        for n in dir(socket)
+        if n.startswith(prefix)
+    )
+
+# this example is more 'pythonic' for 2.7 and above (where dictionary
+# comprehensions exist)
+def get_constants_27(prefix):
+    return {
+        getattr(socket, n):n for n in dir(socket)
+        if n.startswith(prefix)
+    }
 
 
 def get_address_info(host, port):
