0d8e6cad135b01b69880f935b0ac23a597625d8e
cewing <cris@crisewing.com>
2013-12-27 17:16:58 -0700

    convert to pluggable log buffer

--- resources/session01/echo_client.py
+++ resources/session01/echo_client.py
@@ -2,14 +2,14 @@ import socket
 import sys
 
 
-def client(msg):
+def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print >>sys.stderr, 'connecting to {0} port {1}'.format(*server_address)
+    print >>log_buffer, 'connecting to {0} port {1}'.format(*server_address)
     sock.connect(server_address)
     try:
         # Send data
-        print >>sys.stderr, 'sending "{0}"'.format(msg)
+        print >>log_buffer, 'sending "{0}"'.format(msg)
         sock.sendall(msg)
         # Look for the response
         amount_received = 0
@@ -17,9 +17,9 @@ def client(msg):
         while amount_received < amount_expected:
             data = sock.recv(16)
             amount_received += len(data)
-            print >>sys.stderr, 'received "{0}"'.foramt(data)
+            print >>log_buffer, 'received "{0}"'.foramt(data)
     finally:
-        print >>sys.stderr, 'closing socket'
+        print >>log_buffer, 'closing socket'
         sock.close()
 
 
--- resources/session01/echo_server.py
+++ resources/session01/echo_server.py
@@ -2,30 +2,30 @@ import socket
 import sys
 
 
-def server():
+def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print >>sys.stderr, "making a server on {0}:{1}".format(*address)
+    print >>log_buffer, "making a server on {0}:{1}".format(*address)
     sock.bind(address)
     sock.listen(1)
     
     try:
         while True:
-            print >>sys.stderr, 'waiting for a connection'
+            print >>log_buffer, 'waiting for a connection'
             conn, addr = sock.accept() # blocking
             try:
-                print >>sys.stderr, 'connection - {0}:{1}'.format(*addr)
+                print >>log_buffer, 'connection - {0}:{1}'.format(*addr)
                 while True:
                     data = conn.recv(16)
-                    print >>sys.stderr, 'received "{0}"'.format(data)
+                    print >>log_buffer, 'received "{0}"'.format(data)
                     if data:
                         msg = 'sending data back to client'
-                        print >>sys.stderr, msg
+                        print >>log_buffer, msg
                         conn.sendall(data)
                     else:
                         msg = 'no more data from {0}:{1}'.format(addr)
-                        print >>sys.stderr, msg
+                        print >>log_buffer, msg
                         break
             finally:
                 conn.close()
