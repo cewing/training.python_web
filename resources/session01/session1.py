d95ca2b2350ce8e57a6b15971c8b33ad3472a511
cewing <cris@crisewing.com>
2013-12-27 17:41:12 -0700

    remove answers from master branch for session 01

--- resources/session01/session1.py
+++ resources/session01/session1.py
@@ -1,16 +1,5 @@
 import socket
 
-
-def get_named_services(first_port=0, last_port=2**15+2**14):
-    tmpl = '{0: >4}: {1}'
-    for i in xrange(first_port, last_port):
-        try:
-            service = socket.getservbyport(i)
-        except socket.error:
-            continue
-        print tmpl.format(i, service)
-
-
 def get_constants(prefix):
     return dict( 
         (getattr(socket, n), n)
@@ -21,10 +10,7 @@ def get_constants(prefix):
 # this example is more 'pythonic' for 2.7 and above (where dictionary
 # comprehensions exist)
 def get_constants_27(prefix):
-    return {
-        getattr(socket, n):n for n in dir(socket)
-        if n.startswith(prefix)
-    }
+    return {getattr(socket, n):n for n in dir(socket) if n.startswith(prefix)}
 
 
 def get_address_info(host, port):
@@ -39,13 +25,3 @@ def get_address_info(host, port):
         print 'canonical name: ', nam
         print 'socket address: ', add
         print
-
-
-def print_address_constants():
-    addrs = get_constants('INADDR')
-    for val, name in addrs.items():
-      hexval = hex(val)
-      packed = socket.inet_aton(hexval)
-      addr = socket.inet_ntoa(packed)
-      tmpl = '{0: >24}: {1: <10} {2: <16}'
-      print tmpl.format(name, hexval, addr)
