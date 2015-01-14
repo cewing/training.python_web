0509669471ba6fa6581174d457cae26275374c8d
cewing <cris@crisewing.com>
2015-01-13 22:17:34 -0800

    make Entry object classmethods more usable in the interpreter.

--- resources/session02/models.py
+++ resources/session02/models.py
@@ -41,15 +41,19 @@ class Entry(Base):
     edited = Column(DateTime, default=datetime.datetime.utcnow)
 
     @classmethod
-    def all(cls):
+    def all(cls, session=None):
         """return a query with all entries, ordered by creation date reversed
         """
-        return DBSession.query(cls).order_by(sa.desc(cls.created)).all()
+        if session is None:
+            session = DBSession
+        return session.query(cls).order_by(sa.desc(cls.created)).all()
 
     @classmethod
-    def by_id(cls, id):
+    def by_id(cls, id, session=None):
         """return a single entry identified by id
 
         If no entry exists with the provided id, return None
         """
-        return DBSession.query(cls).get(id)
+        if session is None:
+            session = DBSession
+        return session.query(cls).get(id)
