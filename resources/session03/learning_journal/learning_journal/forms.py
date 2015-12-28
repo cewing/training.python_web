d65954b954611ed5cc38ede74d186ab312ecc2a1
cewing <cris@crisewing.com>
2015-12-28 13:14:06 -0800

    pep8 fix

--- resources/session03/learning_journal/learning_journal/forms.py
+++ resources/session03/learning_journal/learning_journal/forms.py
@@ -26,6 +26,7 @@ class EntryCreateForm(Form):
 class EntryEditForm(EntryCreateForm):
     id = HiddenField()
 
+
 class LoginForm(Form):
     username = TextField('Username', [validators.Length(min=1, max=255)])
     password = PasswordField('Password', [validators.Length(min=1, max=255)])
