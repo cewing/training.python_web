907ed712b56354268e5c5a4add3be4e6b431e2b1
cewing <cris@crisewing.com>
2015-12-30 13:57:19 -0800

    update cgi scripts to fit py3 usage

--- resources/session06/cgi/cgi-bin/cgi_sums.py
+++ resources/session06/cgi/cgi-bin/cgi_sums.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import cgi
 import cgitb
-import json
 
 cgitb.enable()
 
-print "Content-type: text/plain"
-print
-print "Your job is to make this work"
+print("Content-type: text/plain")
+print()
+print("Your job is to make this work")
