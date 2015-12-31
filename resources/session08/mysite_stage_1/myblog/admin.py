719083e7b774d883828ffe47515b2cc83c6e57f0
cewing <cris@crisewing.com>
2015-12-31 13:55:44 -0800

    update stage 1 blog app to a 1.9 version

--- resources/session08/mysite_stage_1/myblog/admin.py
+++ resources/session08/mysite_stage_1/myblog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from myblog.models import Post, Category
+
+from myblog.models import Category
+from myblog.models import Post
 
 
-admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Post)
--- resources/session08/mysite_stage_1/myblog/tests.py
+++ resources/session08/mysite_stage_1/myblog/tests.py
@@ -1,25 +1,27 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from myblog.models import Post, Category
+
+from myblog.models import Category
+from myblog.models import Post
 
 
 class PostTestCase(TestCase):
-    fixtures = ['myblog_test_fixture.json', ]
+    fixtures = ['myblog_test_fixture.json']
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
 
-    def test_unicode(self):
+    def test_string_representation(self):
         expected = "This is a title"
         p1 = Post(title=expected)
-        actual = unicode(p1)
+        actual = str(p1)
         self.assertEqual(expected, actual)
 
 
 class CategoryTestCase(TestCase):
 
-    def test_unicode(self):
+    def test_string_representation(self):
         expected = "A Category"
         c1 = Category(name=expected)
-        actual = unicode(c1)
+        actual = str(c1)
         self.assertEqual(expected, actual)
